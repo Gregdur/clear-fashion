72f5c135502fe182f74ed5b5abe13e62cf363474
Gregdur <gregoirecaurier@gmail.com>
2022-02-14 13:51:41 +0100

    scraping all products v1

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+//const dedicatedbrand_scrape=dedicatedbrand.scrape;
+const dedicatedbrand_len=dedicatedbrand.NumberOfProduct;
+
 const loom = require('./sources/loom');
 
 // async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
@@ -17,20 +20,52 @@ const loom = require('./sources/loom');
 //    }
 //  }
 
- async function sandbox (eshop = 'https://www.loom.fr/collections/hauts-homme') {
-  try {
+//  async function sandbox (eshop = 'https://www.loom.fr/collections/hauts-homme') {
+//   try {
+//      console.log(`🕵️‍♀️  browsing ${eshop} source`);
+
+//     const products = await loom.scrape(eshop);
+
+//     console.log(products);
+//     console.log('done');
+//     process.exit(0);
+//    } catch (e) {
+//      console.error(e);
+//      process.exit(1);
+//    }
+//  }
+
+
+async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/all-men?p=') {
+   try {
      console.log(`🕵️‍♀️  browsing ${eshop} source`);
+     let allproduct=[];
+     for(let i=1; ;i++){
+        const product = await dedicatedbrand.scrape(eshop + i.toString());
+        const len=product.length;
+        //allproduct=allproduct.concat(product);
+        if (len!=0){
+          //console.log(allproduct);
+          //allproduct=allproduct.concat(product);
+          console.log(product);
+        }
+        else{                    
+          //console.log(allproducts);
+          console.log('done');
+          process.exit(0);
+        }
 
-    const products = await loom.scrape(eshop);
 
-    console.log(products);
-    console.log('done');
-    process.exit(0);
-   } catch (e) {
+    };
+
+     
+   } 
+   catch (e) {
      console.error(e);
      process.exit(1);
    }
  }
+
 
 
 const [,, eshop] = process.argv;
--- server/sources/dedicatedbrand.js
+++ server/sources/dedicatedbrand.js
@@ -15,8 +15,7 @@ const parse = data => {
         .find('.productList-title')
         .text()
         .trim()
-        .replace(/\s/g, ' ');
-      const 
+        .replace(/\s/g, ' ');       
       const price = parseInt(
         $(element)
           .find('.productList-price')
@@ -27,6 +26,19 @@ const parse = data => {
     })
     .get();
 };
+
+
+// const NumberOfProduct = async url =>{
+//   const response = await fetch(url);
+//   if (response.ok) {
+
+//           const body = await response.text();
+//           const result = parse(body);
+//           const nbPage=Math.ceil(result[0]["nbProduct"]/result[0]["nbCurrent"]);
+//           return nbPage;
+// }
+
+// module.exports.NumberOfProduct();
 
 /**
  * Scrape all the products for a given url page
@@ -51,3 +63,57 @@ module.exports.scrape = async url => {
     return null;
   }
 };
+
+
+// /**
+//  * Scrape all the products for a given url page
+//  * @param  {[type]}  url
+//  * @return {Array|null}
+//  */
+//  module.exports.scrape = async url => {
+//   try {
+//     const response = await fetch(url);
+
+//     if (response.ok) {
+
+//       const body = await response.text();
+//       const result = parse(body);
+//       const nbproduct=result[0]["nbCurrent"];
+//       let i=1;
+//       let allproduct=[];
+
+//       while(nbproduct!=0){
+//         const url2='https://www.dedicatedbrand.com/en/men/all-men?p='+i.toString();
+//         try{
+//           const response2=await fetch(url2);
+
+//           if (response2.ok){
+//             const body = await response2.text();
+//             allproduct=allproduct.concat(parse(body2))                   
+//           }
+//           else{
+//             console.error(response2)
+//             return null
+//           }
+//         }catch(error){
+//           console.error(error);
+//           return null;
+//         }
+
+//         i++;
+//       }
+
+
+
+//       return parse(body);
+//     }
+
+//     console.error(response);
+
+//     return null;
+//   } catch (error) {
+//     console.error(error);
+//     return null;
+//   }
+// };
+
